55fd0a98bd021ad7e3f15863c0ce45b2cea96c4d
Agustin Cabrera <58793527+AgustinCabrera@users.noreply.github.com>
2024-12-01 19:50:45 -0300

    prisma rollback

--- dattle/src/app/pages/api/animals/index.ts
+++ dattle/src/app/pages/api/animals/index.ts
@@ -1,20 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import  knexInstance  from "@/lib/knex";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const { identification, birthDate, breed, gender } = req.body;
     try {
-      const [newAnimal] = await knexInstance("Animal")
-        .insert({ identification, birthDate, breed, gender })
-        .returning("*");
-        
+      const newAnimal = await prisma.animal.create({
+        data: {
+          identification,
+          birthDate: new Date(birthDate),
+          breed,
+          gender,
+        },
+      });
       res.status(201).json({ success: true, data: newAnimal });
     } catch (error) {
       console.error("Error creating animal:", error);
       res.status(500).json({ success: false, message: "Error creating animal" });
     }
-  
   } else {
     res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
